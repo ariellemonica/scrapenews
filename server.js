2210ca0e51839e5386f8510f4f64095b0143b781
Monica Nadal <missmonicanadal@gmail.com>
2020-06-16 18:22:26 -0700

    ugghh

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ var mongoose = require("mongoose");
 
 var axios = require("axios");
 var cheerio = require("cheerio");
-var db = require("./models");
 
 var PORT = 3030;
 
@@ -13,6 +12,8 @@ var app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(logger("dev"));
+
+var db = require("./models");
 
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 mongoose.connect(MONGODB_URI);
